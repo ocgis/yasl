3bc11efe9554f537c76248aac1c7fa00b61d2ddd
Christer Gustavsson <cg@nocrew.org>
2015-04-04 16:49:06 +0200

    Added some debugging code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_flags
+
+  private
+
+  def set_flags
+    flags = { debug: false }
+    flags.each do |k, v|
+      unless params[k].nil?
+        if params[k] == "true"
+          flags[k] = true
+        end
+      end
+    end
+
+    @flags = OpenStruct.new(flags)
+  end
+
 end
--- app/helpers/selections_helper.rb
+++ app/helpers/selections_helper.rb
@@ -1,2 +1,22 @@
 module SelectionsHelper
+
+  def selection_sortindex(now, a)
+    return (now - a.updated_at) / ((a.updated_at - a.first_bought_at) / (a.buycounter - 1))
+  end
+
+  def selection_name(item)
+    item_name = item.name
+    if @flags.debug
+      if item.buycounter > 1
+        now = Time.now
+        sortindex = selection_sortindex(now, item)
+        item_name << " sortindex: #{sortindex}"
+        avg = (item.updated_at - item.first_bought_at) / (item.buycounter - 1)
+        item_name << " avg: #{avg}"
+        since = now - item.updated_at
+        item_name << " since: #{since}"
+      end
+    end
+    return item_name
+  end
 end
