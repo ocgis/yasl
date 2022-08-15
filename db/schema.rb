71b2d816ed88e199ffb1ecf1eca97093ce8a5a8c
Christer Gustavsson <cg@nocrew.org>
2022-08-15 18:46:53 +0200

    Ran rails app:update.

--- db/schema.rb
+++ db/schema.rb
@@ -1,41 +1,38 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312194417) do
-
-  create_table "items", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "buycounter",      default: 0
-    t.datetime "first_bought_at"
+ActiveRecord::Schema[7.0].define(version: 2022_08_15_163845) do
+  create_table "items", force: :cascade do |t|
+    t.string "name", limit: 255
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
+    t.integer "buycounter", default: 0
+    t.datetime "first_bought_at", precision: nil
   end
 
-  create_table "lists", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "lists", force: :cascade do |t|
+    t.string "name", limit: 255
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
-  create_table "selections", force: true do |t|
-    t.string   "status"
-    t.integer  "list_id"
-    t.integer  "item_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "selections", force: :cascade do |t|
+    t.string "status", limit: 255
+    t.integer "list_id"
+    t.integer "item_id"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
+    t.index ["item_id"], name: "index_selections_on_item_id"
+    t.index ["list_id"], name: "index_selections_on_list_id"
   end
-
-  add_index "selections", ["item_id"], name: "index_selections_on_item_id"
-  add_index "selections", ["list_id"], name: "index_selections_on_list_id"
 
 end
